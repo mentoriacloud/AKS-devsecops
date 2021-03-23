9242b056ebac952f78c421b8409da7001a3cb631
Fabiano Jamati <fabiano_jamati@yahoo.com.br>
2021-03-23 14:51:09 -0300

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,8 @@
+const apiKey ="12345678912345678941212654-12345123-165132456-1234556879";
+const db = "zyz987654312.database.secure.windows.net"
+const storageConn = "DefaultEndpointsProtocol=https;AccountName=joaozinhodobi;AccountKey=jfkakdjfksjithxihfjskfixKbbMCShAU08al+UmDbHVzTrxUtgZxAx1lDNcFfktGLlrfJPSt+VWnKriS8TqEwEFA==;EndpointSuffix=core.windows.net"
+
+function endsWith(x, y) {
+    let index = x.lastIndex0f(y);
+    return index !== -1 && index === x.length - y.length;
+}
